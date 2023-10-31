8c274bb60ddfbc1756d6d87af59448d3714c8a2a
Valera <104449560+Valera024@users.noreply.github.com>
2023-10-31 17:23:36 +0200

    2.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,13 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe)
-
+console.log("test")
 const callback = throttle(function (data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
 }, 1000);
 
 player.on("timeupdate", callback);
 
-const savedTime = localStorage.getItem("videoplayer-current-time");
+const savedTime = localStorage.getItem("videoplayer-current-time") || 0;
 
 player.setCurrentTime(parseFloat(savedTime)).then(function (seconds){});
